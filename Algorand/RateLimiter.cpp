4110b6c3196156884308e797cad86d99f21b8e20
Lachu Jeethu <jeethjayakumar@Lachu-Jeethu.local>
2022-08-05 01:29:35 -0400

    Updated code to support multiple client

--- Algorand/RateLimiter.cpp
+++ Algorand/RateLimiter.cpp
@@ -2,56 +2,47 @@
 #include <stdio.h>
 #include <algorithm>
 #include <queue>
+#include <map>
 
 using namespace std;
 
-/*
+
 class ClientRequestInfo
 {
 	int clientID;
 	int timeID;
 public:
-	ClientRequestInfo(int clientID, int timeID)
-	{
+	ClientRequestInfo(int clientID, int timeID){
 		this->clientID = clientID;
 		this->timeID = timeID;
 	}
 
-	~ClientRequestInfo()
-	{
-
-	}
+	~ClientRequestInfo(){ }
 	
-	int getTimeID()
-	{
+	int getTimeID(){
 		return timeID;
 	}
 
-	int getClientID()
-	{
+	int getClientID(){
 		return clientID;
 	}
 };
-*/
+
 
 // ASSUMPTION: Currently this solution is implemented for single client.
 //             Will modify the code to support multiple client. 
 //	       Basic logic remains the same.
-bool rateLimiter(int &reqTime, queue<int> &timeQueue)
-{
-	if (timeQueue.empty())
-	{
+bool rateLimiter(int &reqTime, queue<int> &timeQueue){
+	if (timeQueue.empty()){
 		timeQueue.push(reqTime);
 		return true;
 	}
 	
-	if (timeQueue.size() == 3)
-	{
+	if (timeQueue.size() == 3){
 		int timeDiff = reqTime - timeQueue.front();
 		if (timeDiff < 1000)
 			return false;
-		else
-		{
+		else{
 			timeQueue.pop();
 			timeQueue.push(reqTime);
 		}
@@ -61,16 +52,38 @@ bool rateLimiter(int &reqTime, queue<int> &timeQueue)
 	return true;
 }
 
+void initializeClientReqCounter(int &clientCount, map<int, queue<int> > &clientRequestCount, queue<int> reqTimeInfo[]){
+	int tmpClientID;
+	for (int i = 0; i < clientCount; i++){
+		cout<<"Enter client ID: ";
+		cin>>tmpClientID;
+		clientRequestCount.insert(make_pair(tmpClientID, reqTimeInfo[i]));
+	}
+}
+
+// This method will help us handle multiple clients!!!
+bool checkRequestStatus(ClientRequestInfo reqInfo, map<int, queue<int> > &clientRequestCount){
+	int cID = reqInfo.getClientID();
+	if (clientRequestCount.find(cID) == clientRequestCount.end()){
+		cout<<"Client ID: "<<cID<<" not registered!!! Request failed!!!\n";
+		return false;
+	}
+
+	queue<int> &tmp = clientRequestCount[cID];
+	return rateLimiter(reqInfo.getTimeID(), tmp);
+}
+
 int main()
 {
-	//int clientCount;
-	//cout<<"Enter number of Clients: ";
-	//cin>>clientCount;
+	int clientCount;
+	cout<<"Enter number of Clients: ";
+	cin>>clientCount;
 
-	//ClientRequestInfo clients[clientCount];
-	//map<int, queue<int>> clientRequestCount;
-
-	queue<int> reqTimeInfo;
+	map<int, queue<int> > clientRequestCount;
+	queue<int> reqTimeInfo[clientCount];
+	
+	initializeClientReqCounter(clientCount, clientRequestCount, reqTimeInfo);
+/*	
 	int count = 0;
 	int reqTime = 100;
 	while (count < 20)
@@ -79,5 +92,8 @@ int main()
 		reqTime += 100;
 		count ++;
 	}
+*/
+	ClientRequestInfo *tmpClient = new ClientRequestInfo(100, 100);
+	cout<<"ClientID: "<<tmpClient->getClientID()<<" Request Status: "<<checkRequestStatus(*tmpClient, clientRequestCount)<<endl;
 	return 0;
 }
